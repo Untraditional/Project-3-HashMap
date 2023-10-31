9c21343c653b6c792280cd1760a6115ec37fe3ee
Untraditional <58677398+Untraditional@users.noreply.github.com>
2023-10-31 09:59:02 -0400

    Added a file browser
    Added the ability to choose a .java file and then display what is in the file in the textArea in the gui.

--- Project 3/src/project3/Project3FXMLController.java
+++ Project 3/src/project3/Project3FXMLController.java
@@ -4,23 +4,58 @@
  */
 package project3;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Scanner;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 
-/**
- * FXML Controller class
- *
- * @author Alex
- */
+
 public class Project3FXMLController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    FileChooser fileChooser = new FileChooser();
+    @FXML
+    private Button inputFileButton;
+    @FXML
+    private TextArea textArea;
+    @FXML
+    private Button inputCompareFile;
+
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+       fileChooser.setInitialDirectory(new File("C:\\Users\\Alex\\Documents\\UMD Classes\\Fall 2023\\CIS 296 - Java Programming\\Homework\\Project-3-HashMap\\Project 3\\src\\project3"));
     }    
+
+    @FXML
+    private void inputFileToProgram(ActionEvent event) {
+        fileChooser.getExtensionFilters().add(new ExtensionFilter("Java Files","*.java"));
+        File file = fileChooser.showOpenDialog(null);
+        if (file != null){
+            try{
+                Scanner scanner = new Scanner(file);
+                StringBuilder fileCont = new StringBuilder();
+                while (scanner.hasNextLine()){
+                    fileCont.append(scanner.nextLine()).append("\n");
+                }
+                scanner.close();
+                textArea.setText(fileCont.toString());
+            }
+            catch (FileNotFoundException e){
+                e.printStackTrace();
+            }
+            
+            
+        }
+        
+    }
+    
     
 }
--- Project 3/src/project3/project3.java
+++ Project 3/src/project3/project3.java
@@ -2,13 +2,20 @@
 package project3;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 
 public class project3 extends Application{
     @Override
     public void start(Stage stage) throws Exception {
-        
+        Parent root = FXMLLoader.load(getClass().getResource("project3FXML.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Project 3 - Hash Map");
+        stage.setScene(scene);
+        stage.show();
     }
     public static void main(String[] args) {
         launch(args);
