a046057d52a618c9362f46968db3a6748483d7ca
Untraditional <58677398+Untraditional@users.noreply.github.com>
2023-10-31 07:56:27 -0400

    Setting up project files

---
+++ Project 3/src/project3/project3.java
@@ -0,0 +1,16 @@
+
+package project3;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class project3 extends Application{
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+}
